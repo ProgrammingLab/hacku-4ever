4c766070bad13e9795f6f67239f929b7b91d2a8d
Taisuke Miyazaki <imishinist@gmail.com>
2016-11-15 19:24:16 +0900

    時間の取得をnumber pickerに

--- app/src/main/java/com/kurume_nct/himawari/TimePickerDialogFragment.java
+++ app/src/main/java/com/kurume_nct/himawari/TimePickerDialogFragment.java
@@ -1,15 +1,19 @@
 package com.kurume_nct.himawari;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.widget.TimePicker;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.NumberPicker;
 
-import java.util.Calendar;
+public class TimePickerDialogFragment extends BaseDialogFragment {
 
-public class TimePickerDialogFragment extends BaseDialogFragment implements TimePickerDialog.OnTimeSetListener{
+    private View hour;
+    private View minute;
 
     public static TimePickerDialogFragment newInstance(Fragment fragment, int requestCode) {
         TimePickerDialogFragment dialog = new TimePickerDialogFragment();
@@ -17,25 +21,52 @@ public class TimePickerDialogFragment extends BaseDialogFragment implements Time
         return dialog;
     }
 
+    private View createPickerView(int hourValue, int minuteValue) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.time_picker, null);
+
+        NumberPicker hour = (NumberPicker) view.findViewById(R.id.picker_hour);
+        hour.setMinValue(0);
+        hour.setMaxValue(23);
+        hour.setValue(hourValue);
+        this.hour = hour;
+
+        NumberPicker minute = (NumberPicker) view.findViewById(R.id.picker_minute);
+        minute.setMinValue(0);
+        minute.setMaxValue(59);
+        minute.setValue(minuteValue);
+        this.minute = minute;
+
+        return view;
+    }
+
+    private int getHour() {
+        NumberPicker picker = (NumberPicker) hour;
+        return picker.getValue();
+    }
+
+    private int getMinute() {
+        NumberPicker picker = (NumberPicker) minute;
+        return picker.getValue();
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final Calendar c = Calendar.getInstance();
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-        int minute = c.get(Calendar.MINUTE);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(createPickerView(0, 0));
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String val = String.format("%d:%d", getHour(), getMinute());
+                OnValueSetListener listener = getListener();
+                if (listener != null) {
+                    listener.onValueSet(getTargetRequestCode(), val);
+                }
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.setCancelable(true);
 
-        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), this, hour, minute, true);
-
-        return timePickerDialog;
-    }
-
-    @Override
-    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-        String val = String.format("%d:%d", hour, minute);
-        try {
-            getListener().onValueSet(getTargetRequestCode(), val);
-        } catch(NullPointerException e) {
-            e.printStackTrace();
-        }
+        return builder.create();
     }
 }
