b9cbf462b70b14264e50c37e8cf7c100277c57dc
Taisuke Miyazaki <imishinist@gmail.com>
2016-11-15 19:34:07 +0900

    dialogの作り方を修正

--- app/src/main/java/com/kurume_nct/himawari/PriceDialogFragment.java
+++ app/src/main/java/com/kurume_nct/himawari/PriceDialogFragment.java
@@ -26,13 +26,18 @@ public class PriceDialogFragment extends BaseDialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final View v = getActivity().getLayoutInflater().inflate(R.layout.fragment_dialog_input, null);
         String title = getArguments().getString("TITLE");
-
-        return new AlertDialog.Builder(getActivity()).setTitle(title).setView(v).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(v);
+        builder.setTitle(title);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 EditText text = (EditText) v.findViewById(R.id.editText);
                 getListener().onValueSet(getTargetRequestCode(), text.getText().toString().trim());
             }
-        }).create();
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.setCancelable(true);
+        return builder.create();
     }
 }
